203ea0754798a18229e297cb6fe3ab168a854cb4
Akynyaz64 <mr.akynyaz29@gmail.com>
2023-06-27 09:58:01 +0500

    login and main page admin

--- src/pages/admin/auth/Login.jsx
+++ src/pages/admin/auth/Login.jsx
@@ -1,16 +1,13 @@
-import { useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-hot-toast";
-import { useAuth } from "../../../context/AuthContext";
+import {useContext, useRef, useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEye} from "@fortawesome/free-solid-svg-icons";
+import AuthContext from "../../../context/AuthContext";
 import login_img from "../../../assets/auth/login.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye } from "@fortawesome/free-solid-svg-icons";
-import not_see from '../../../assets/icons/not-see.svg'
+import not_see from "../../../assets/icons/not-see.svg";
 
 const Login = () => {
-    const { setAdmin, setIsLoggedIn } = useAuth();
+    const {login} = useContext(AuthContext);
     const [isSubmitting, setIsSubmitting] = useState(false);
-    const navigate = useNavigate();
     const email = useRef("");
     const password = useRef("");
 
@@ -19,46 +16,14 @@ const Login = () => {
 
         setIsSubmitting(true);
 
-        const loginData = {
+        let payload = {
             email: email.current.value,
             password: password.current.value,
         };
-        const response = await fetch(`/admin-api/account/login`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(loginData),
-        });
-
-        const resData = await response.json();
-
-        if (resData.status === false) {
-            toast.error(resData.message);
-            setIsSubmitting(false);
-            return null;
-        }
-
-        if (resData.status === true) {
-            toast.success(resData.message);
-            setIsSubmitting(false);
-            const token = resData.data.token;
-            localStorage.setItem("adACto", token);
-
-            setIsLoggedIn(true);
-
-            setAdmin({
-                id: 1,
-                role: "Admin",
-                status: true,
-            });
-
-            navigate("/admin");
-        }
+        await login(payload);
 
         setIsSubmitting(false);
     };
-
 
     //PASSWORD SHOW
     const [isVisible1, setVisible1] = useState(false);
@@ -91,8 +56,8 @@ const Login = () => {
                                                         <div className="floating-label form-group input-group">
                                                             <input className="floating-input form-control " type={!isVisible1 ? "password" : "text"} id="password" name="password" placeholder=" " ref={password} required />
                                                             <label className="top-0 px-2 bg-white">Parol</label>
-                                                            <span className="input-group-text bg-white border-start-0" style={{ cursor: "pointer" }} onClick={toggle1}>
-                                                                {isVisible1 ? <FontAwesomeIcon icon={faEye} className='text-muted' /> : <img src={not_see} alt="" className='img-fluid' />}
+                                                            <span className="input-group-text bg-white border-start-0" style={{cursor: "pointer"}} onClick={toggle1}>
+                                                                {isVisible1 ? <FontAwesomeIcon icon={faEye} className="text-muted" /> : <img src={not_see} alt="icon" className="img-fluid" />}
                                                             </span>
                                                         </div>
                                                     </div>
@@ -104,7 +69,7 @@ const Login = () => {
                                         </div>
                                     </div>
                                     <div className="col-xl-5">
-                                        <img src={login_img} alt="" className="img-fluid" />
+                                        <img src={login_img} alt="login" className="img-fluid" />
                                     </div>
                                 </div>
                             </div>
