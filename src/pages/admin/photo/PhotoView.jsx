1a2a800e7143c51ce783778818f595747fd00584
Akynyaz64 <mr.akynyaz29@gmail.com>
2023-06-27 10:02:39 +0500

    post page crud

---
+++ src/pages/admin/photo/PhotoView.jsx
@@ -0,0 +1,110 @@
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import {Loader} from "../../../components";
+import { toast } from "react-hot-toast";
+
+const PhotoView = () => {
+    const {photoId} = useParams();
+
+    const [photo, setPhoto] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+
+    function formatDate(data) {
+        let date = new Date(data);
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
+        let day = date.getDate() + 1;
+        if (day < 10) {
+            day = "0" + day;
+        }
+        if (month < 10) {
+            month = "0" + month;
+        }
+        date = day + "." + month + "." + year + " ";
+        return date;
+    }
+
+    const fetchData = async () => {
+        setIsLoading(true);
+        const response = await fetch(`/admin-api/gallery/${photoId}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("adACto")}`,
+            },
+        });
+
+        if (!response.ok) {
+            setIsLoading(false);
+            toast.error("Error status: " + response.statusText);
+            return null;
+        }
+        const resData = await response.json();
+        console.log(resData);
+        setPhoto(resData.data);
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div className="container-fluid">
+            <div className="row justify-content-center">
+                <div className="col-lg-8">
+                    <div className="d-flex flex-wrap align-items-center justify-content-between mb-4">
+                        <h3 className="mb-3">About photo</h3>
+                    </div>
+                </div>
+                {isLoading ? (
+                    <Loader />
+                ) : (
+                    <div className="col-lg-8 mt-3">
+                        <div className="form-row">
+                            <div className="col-md-12 mb-4">
+                                <img alt="photo" src={"/" + photo?.avatar_image?.url} className="img-fluid rounded" />
+                            </div>
+                            {photo.images?.map((image, index) => (
+                                <div key={index} className="col-md-3 mb-4">
+                                    <img alt="photo" src={"/" + image.url} className="img-fluid rounded" />
+                                </div>
+                            ))}
+                            <div className="col-md-12 mb-3">
+                                <h2>{photo.title}</h2>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <h5>User:</h5>
+                                <p>{photo.user?.name}</p>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <h5>User role:</h5>
+                                <p>{photo.user?.role}</p>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <h5>Publikasiýanyň görnüşi:</h5>
+                                <p>{photo.publication_type?.type}</p>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <h5>Page / Category:</h5>
+                                <ul>
+                                    {photo.page_category?.map((page_category, index) => (
+                                        <li key={index}>
+                                            {page_category.page.name} / {page_category.category.name}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <h5>Goşulan wagty:</h5>
+                                <p>{formatDate(photo.created_at)}</p>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default PhotoView;
---
+++ src/pages/admin/posts/PostView.jsx
@@ -0,0 +1,115 @@
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import {toast} from "react-hot-toast";
+import {Loader} from "../../../components";
+
+const PostView = () => {
+    const {postId} = useParams();
+
+    const [post, setPost] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchData = async () => {
+        setIsLoading(true);
+        const response = await fetch(`/admin-api/post/${postId}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("adACto")}`,
+            },
+        });
+
+        if (!response.ok) {
+            setIsLoading(false);
+            toast.error("Error status: " + response.statusText);
+            return null;
+        }
+        const resData = await response.json();
+        console.log(resData);
+        setPost(resData.data);
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div className="container-fluid">
+            <div className="row justify-content-center">
+                <div className="col-lg-8">
+                    <div className="d-flex flex-wrap align-items-center justify-content-between mb-4">
+                        <h3 className="mb-3">About post</h3>
+                    </div>
+                </div>
+                {isLoading ? (
+                    <Loader />
+                ) : (
+                    <div className="col-lg-8 mt-3">
+                        <div className="form-row">
+                            {post.images?.map((image, index) => (
+                                <div key={index} className="col-md-3 mb-4">
+                                    <img alt="post" src={"/" + image.url} className="img-fluid rounded" />
+                                </div>
+                            ))}
+                            <div className="col-md-12 mb-3">
+                                <h2>{post.title}</h2>
+                            </div>
+                            <div className="col-md-12 mb-3">
+                                <p dangerouslySetInnerHTML={{__html: post.description}}></p>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <h5>Bahasy:</h5>
+                                <p>{post.price} TMT</p>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <h5>Arzanladyş bahasy:</h5>
+                                <p>{post.discount} TMT</p>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <h5>Telefon belgi:</h5>
+                                <p>{post.phone}</p>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <h5>Başlangyç wagty:</h5>
+                                <p>{post.start_date?.slice(0, 10)}</p>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <h5>Soňky wagty:</h5>
+                                <p>{post.end_date?.slice(0, 10)}</p>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <h5>Goşulan wagty:</h5>
+                                <p>{post.created_at?.slice(0, 10)}</p>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <h5>User:</h5>
+                                <p>{post.user?.name}</p>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <h5>Görlen sany:</h5>
+                                <p>{post.viewed_count}</p>
+                            </div>
+                            <div className="col-md-4 mb-3">
+                                <h5>Tassyklanan:</h5>
+                                <p>{post.approved ? "Approved" : "Not approved"}</p>
+                            </div>
+                            <div className="col-md-12 mb-3">
+                                <h5>Hashtags:</h5>
+                                <div className="tags-container mt-3">
+                                    {post.tags?.map((e, index) => (
+                                        <span className="tag" key={index}>
+                                            {e.name + " "}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default PostView;
