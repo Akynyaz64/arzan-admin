9708c2933029a98878fc9dca6fc9b668aacfb4e4
Akynyaz64 <mr.akynyaz29@gmail.com>
2023-07-21 18:05:38 +0500

    add data file

---
+++ src/data/data.js
@@ -0,0 +1,17 @@
+export const filterNames = [
+    {name: "coin", value: "Ball boýunça"},
+    {name: "like", value: "Like sany boýunça boýunça"},
+    {name: "view", value: "Görlen sany boýunça"},
+    {name: "follower", value: "Yzarlaýjylar sany boýunça"},
+    {name: "following", value: "Yzarlaýan sany boýunça"},
+    {name: "post", value: "Arzanladyş sany boýunça"},
+    {name: "video", value: "Wideo sany boýunça"},
+    {name: "image", value: "Surat sany boýunça"},
+    {name: "gallery", value: "Albom sany boýunça"},
+];
+
+export const userTypes = [
+    {id: 1, name: "USER"},
+    {id: 2, name: "OFFICIAL"},
+    {id: 3, name: "EXPIRED"},
+];
